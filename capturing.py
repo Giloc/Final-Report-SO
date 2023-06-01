c0db61bebdf8821dbc603dd9606340c9d23740fc
Giloc <55797247+Giloc@users.noreply.github.com>
2023-05-31 19:30:34 -0500

    v2

--- capturing.py
+++ capturing.py
@@ -1,24 +1,25 @@
 from scapy.all import *
 import psutil
 from collections import defaultdict
-import os
 from threading import Thread
 import pandas as pd
-import subprocess
+import tkinter
 
 all_macs = {iface.mac for iface in ifaces.values()}
 connection2pid = {}
 
 pid2traffic = defaultdict(lambda: [0, 0])
 
-global_df = None
-warning_terminal = None
+global_df = pd.DataFrame()
+
 is_program_running = True
 
 speed_limit = 1024
 
 warning_message = "Process is exceeding the bandwidth limit!"
 standard_message = "OK"
+
+warning_processes = {}
 
 def get_size(bytes):
     for unit in ['', 'K', 'M', 'G', 'T', 'P']:
@@ -27,6 +28,8 @@ def get_size(bytes):
         bytes /= 1024
 
 def process_packet(packet):
+    if not is_program_running:
+        sys.exit()
     global pid2traffic
     try:
         packet_connection = (packet.sport, packet.dport)
@@ -41,7 +44,6 @@ def process_packet(packet):
                 pid2traffic[packet_pid][1] += len(packet)
 
 def get_connections():
-    print("a")
     global connection2pid
     while is_program_running:
         for c in psutil.net_connections():
@@ -50,7 +52,7 @@ def get_connections():
                 connection2pid[(c.raddr.port, c.laddr.port)] = c.pid
         time.sleep(1)
 
-def print_pid2traffic():
+def get_pid2traffic():
     global global_df
     processes = []
 
@@ -72,10 +74,10 @@ def print_pid2traffic():
             process["Upload Speed"] = traffic[0] - global_df.at[pid, "Upload"]
             process["Download Speed"] = traffic[1] - global_df.at[pid, "Download"]
             if process["Download Speed"] > speed_limit:
+                warning_processes[pid] = name
                 process["Message"] = warning_message
             else:
                 process["Message"] = standard_message
-                send_warning_command(pid)
         except (KeyError, AttributeError):
             process["Upload Speed"] = traffic[0]
             process["Download Speed"] = traffic[1]
@@ -89,54 +91,68 @@ def print_pid2traffic():
     except KeyError as e:
         pass
 
-    printing_df = df.copy()
-    try:
-        printing_df["Download"] = printing_df["Download"].apply(get_size)
-        printing_df["Upload"] = printing_df["Upload"].apply(get_size)
-        printing_df["Download Speed"] = printing_df["Download Speed"].apply(get_size).apply(lambda s: f"{s}/s")
-        printing_df["Upload Speed"] = printing_df["Upload Speed"].apply(get_size).apply(lambda s: f"{s}/s")
-    except KeyError as e:
-        pass
-
-    os.system("cls") if "nt" in os.name else os.system("clear")
-
-    print(printing_df.to_string())
-
     global_df = df
 
 def print_stats():
     while is_program_running:
         time.sleep(1)
-        print_pid2traffic()
+        get_pid2traffic()    
 
-def kill_process(pid):
-    print("oeoeoe")
-    os.system(f"taskkill /F /PID {pid}")
+def update_data():
+    if not global_df.empty:
+        printing_df = global_df.copy()
+        try:
+            printing_df["Download"] = printing_df["Download"].apply(get_size)
+            printing_df["Upload"] = printing_df["Upload"].apply(get_size)
+            printing_df["Download Speed"] = printing_df["Download Speed"].apply(get_size).apply(lambda s: f"{s}/s")
+            printing_df["Upload Speed"] = printing_df["Upload Speed"].apply(get_size).apply(lambda s: f"{s}/s")
+        except KeyError as e:
+            pass
+        updated_data = printing_df.to_string()
+        data_listbox.delete("1.0", "end")
+        data_listbox.insert(tkinter.INSERT, updated_data)
+        warning.delete(0, "end")
+        for pid in warning_processes.keys():
+            warning.insert("end", get_message(pid))
+        warning_processes.clear()
+    
+    window.after(1000, update_data)
 
-def show_warning(pid):
-    subprocess.call(f"echo WARNING: Process {pid} is exceeding the bandwidth limit!", shell=True)
+def get_message(pid):
+    return f"Process {pid} ({warning_processes[pid]}) is exceeding the bandwidth limit!"
 
-def send_warning_command(pid):
-    # Tu código para enviar el comando "echo" a la terminal de advertencias
-    #global warning_terminal
-    global warning_terminal
-    command = f'echo El proceso {pid} está excediendo el límite de velocidad!'
-    command_bytes = command.encode('utf-8')
-    warning_terminal.stdin.write(command_bytes + b'\n')
-    warning_terminal.stdin.flush()
+def on_window_close():
+    global is_program_running, sniffer
+    is_program_running = False
+    window.destroy()
+    sys.exit()
 
 if __name__ == "__main__":
-    warning_terminal = subprocess.Popen('start cmd', stdin=subprocess.PIPE, shell=True)
-    time.sleep(2)  # Esperar a que la nueva terminal se abra
-
     printing_thread = Thread(target=print_stats)
     printing_thread.start()
 
     connections_thread = Thread(target=get_connections)
     connections_thread.start()
 
-print("Started sniffing")
+    window = tkinter.Tk()
+    window.title("Netwrok usage by process")
+    window.geometry("1280x720")
+    title_label = tkinter.Label(window, text="Netwrok usage by process")
+    title_label.pack()
+    scrollbar = tkinter.Scrollbar(window)
+    scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+    data_listbox = tkinter.Text(window, yscrollcommand=scrollbar.set, height=20, width=220)
+    data_listbox.pack()
+    
+    warning = tkinter.Listbox(window, height=10, width=220)
+    warning.pack()
+    scrollbar.config(command=data_listbox.yview)
+    update_data()
 
-sniff(prn=process_packet, store=False)
+    sniffing_thread = Thread(target=lambda: sniff(prn=process_packet, store=False))
+    sniffing_thread.start()
 
-is_program_running = False
+    window.protocol("WM_DELETE_WINDOW", on_window_close)
+
+    window.mainloop()
+    is_program_running = False
